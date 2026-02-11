0771ed21829db932048d733e55b6d8fd67ea45cd
agathasouza113 <agathasouzasenai113@gmail.com>
2026-02-11 10:26:33 -0300

    desafio1

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,31 @@ function validar(){
         mensagem.innerText = "Inscrição aceita. Categoria PRO";
         mensagem.style.color = "darkgreen";
 
-    }else if (idade >=16 && nivel <=5){
+    } else if (idade >=16 && nivel <=5){
         mensagem.innerText = "Inscrição aceita. Categoria INICIANTE"
         mensagem.style.color = "darkblue"
     }else{
         mensagem.innerText = "Inscrição negada. Idade mínima: 16 anos"
         mensagem.style.color = "red";
     }
+}
+// Desafio 1
+function validar_desafio1(){
+    const senha1 = document.getElementById('senha1').value;
+    const senha2 = document.getElementById('senha2').value;
+    const mensagem1 = document.getElementById("mensagem1");
 
+    if(senha1.length >= 8){
+        if(senha1==senha2){
+            mensagem1.innerText = "Senha confirmada";
+            mensagem1.style.color = "darkgreen";
+        }else{
+            mensagem1.innerText = "Senhas diferentes";
+            mensagem1.style.color = "red";
+        }
+    }else{
+        mensagem1.innerText = "Senha inválida";
+        mensagem1.style.color = "red";
+    }
 }
     
